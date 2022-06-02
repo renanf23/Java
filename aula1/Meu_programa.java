211993fc7b0e0a31fcfc0551b7e13935dd6666d5
Renan Ferreira <101736583+renanf23@users.noreply.github.com>
2022-06-02 10:55:47 -0300

    Initial commit

---
+++ aula1/Meu_programa.java
@@ -0,0 +1,14 @@
+class Meu_programa
+{	
+	public static void main(String entrada[])
+	{
+		int idade = 47;
+		char caracter = 'F';
+		double altura= 1.65;
+		String frase = "Lucy Mari";
+		
+		System.out.println("Eu sou a" + frase + " tenho " + idade + " anos de idade e " + altura + "m de altura");
+			System.exit(0);
+	}
+}
+		
